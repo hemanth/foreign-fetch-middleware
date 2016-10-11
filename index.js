7f0aedd3791e43a143a55afb2e954f42c2e202ed
Hemanth.HM <hemanth.hm@gmail.com>
2016-10-11 19:15:19 +0530

    Fixing typeError, app.use requires a function.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'use strict';
 var fileExists = require('fs').exists;
-module.exports = FSWpath => {
+module.exports = FSWpath => (req, res, next) => {
   fileExists(FSWpath, (exists) => {
     if (exists) {
-      return (req, res, next) => {
         res.setHeader('Link', `</${FSWpath}>; rel="serviceworker"`);
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
-      }
     } else {
       throw new Error(`SW NOT FOUND! Expecting a service work at: ${path}`);
     }
